12b732881de9775ddc81dc1c38ef5505086e7cf5
Nathan Breit <nath@nathanathan.com>
2018-06-26 19:12:48 +0000

    fix missing indent

--- .scripts/download_india_moh_data.py
+++ .scripts/download_india_moh_data.py
@@ -69,62 +69,62 @@ if not (feed and datetime.datetime.now() - datetime.timedelta(10) < feed['addedD
     }, upsert=True)
 
 
-# Reports all appear to use a 6 day long week.
-for week_num, period in enumerate(pd.date_range("2018-01-01", datetime.datetime.now(), freq="6D", closed="left")):
-    week_num = week_num + 1
-    filename = "%02d%s.pdf" % (week_num, period.to_period().start_time.year)
-    url = "http://idsp.nic.in/WriteReadData/l892s/" + filename
-    filepath = os.path.join(args.download_dir, filename)
-    if not os.path.exists(filepath):
-        result = subprocess.call(["wget", url, "-O", filepath])
-        if result != 0:
-            break
-        subprocess.call(["pdftotext", filepath])
-    with open(filepath.replace(".pdf", ".txt")) as f:
-        regex = re.compile(
-            r"(\w{2,}\/\w{2,}\/\d{4}/\d+/\d+)\s+"
-            r"(?P<state>\S(.+\n)+)\s+"
-            r"(?P<district>\S(.+\n)+)\s+"
-            r"(?P<disease>\S(.+\n)+)\s+"
-            r"(?P<cases>\d+)\s+"
-            r"(?P<deaths>\d+)\s+"
-            r"(?P<start>\S(.+\n)+)\s+"
-            r"(?P<reported>\d+\-\d+\-\d+)\s+"
-            r"(?P<status>\S(.+\n)+)\s+")
-        for match in regex.finditer(f.read()):
-            groups = match.groupdict()
-            location_name = clean(groups['state']) + ", " + clean(groups['district'])
-            geoname = lookup_geoname(location_name)
-            if geoname:
-                cases = int(groups['cases'])
-                #print int(groups['deaths'])
-                try:
-                    end_date = datetime.datetime.strptime(
-                        clean(groups['reported']), "%d-%m-%y")
-                except ValueError:
-                    print "Invalid date:", groups['reported']
-                    continue
-                try:
-                    start_date = datetime.datetime.strptime(
-                        clean(groups['start']), "%d-%m-%y")
-                except ValueError:
-                    print "Invalid date:", groups['start']
-                    continue
-                resolved_disease = lookup_disease(clean(groups['disease']))
-                db.counts.insert_one({
-                    "sourceFeed": str(feed['_id']),
-                    "constraining": True,
-                    "dateRange": {
-                        "type": "precise",
-                        "start": start_date,
-                        "end": end_date
-                    },
-                    "locations": [geoname],
-                    "cases": cases,
-                    "resolvedDisease": resolved_disease,
-                    "species": {
-                        "id": "tsn:180092",
-                        "text": "Homo sapiens"
-                    },
-                    "addedDate": datetime.datetime.now()
-                })
+    # Reports all appear to use a 6 day long week.
+    for week_num, period in enumerate(pd.date_range("2018-01-01", datetime.datetime.now(), freq="6D", closed="left")):
+        week_num = week_num + 1
+        filename = "%02d%s.pdf" % (week_num, period.to_period().start_time.year)
+        url = "http://idsp.nic.in/WriteReadData/l892s/" + filename
+        filepath = os.path.join(args.download_dir, filename)
+        if not os.path.exists(filepath):
+            result = subprocess.call(["wget", url, "-O", filepath])
+            if result != 0:
+                break
+            subprocess.call(["pdftotext", filepath])
+        with open(filepath.replace(".pdf", ".txt")) as f:
+            regex = re.compile(
+                r"(\w{2,}\/\w{2,}\/\d{4}/\d+/\d+)\s+"
+                r"(?P<state>\S(.+\n)+)\s+"
+                r"(?P<district>\S(.+\n)+)\s+"
+                r"(?P<disease>\S(.+\n)+)\s+"
+                r"(?P<cases>\d+)\s+"
+                r"(?P<deaths>\d+)\s+"
+                r"(?P<start>\S(.+\n)+)\s+"
+                r"(?P<reported>\d+\-\d+\-\d+)\s+"
+                r"(?P<status>\S(.+\n)+)\s+")
+            for match in regex.finditer(f.read()):
+                groups = match.groupdict()
+                location_name = clean(groups['state']) + ", " + clean(groups['district'])
+                geoname = lookup_geoname(location_name)
+                if geoname:
+                    cases = int(groups['cases'])
+                    #print int(groups['deaths'])
+                    try:
+                        end_date = datetime.datetime.strptime(
+                            clean(groups['reported']), "%d-%m-%y")
+                    except ValueError:
+                        print "Invalid date:", groups['reported']
+                        continue
+                    try:
+                        start_date = datetime.datetime.strptime(
+                            clean(groups['start']), "%d-%m-%y")
+                    except ValueError:
+                        print "Invalid date:", groups['start']
+                        continue
+                    resolved_disease = lookup_disease(clean(groups['disease']))
+                    db.counts.insert_one({
+                        "sourceFeed": str(feed['_id']),
+                        "constraining": True,
+                        "dateRange": {
+                            "type": "precise",
+                            "start": start_date,
+                            "end": end_date
+                        },
+                        "locations": [geoname],
+                        "cases": cases,
+                        "resolvedDisease": resolved_disease,
+                        "species": {
+                            "id": "tsn:180092",
+                            "text": "Homo sapiens"
+                        },
+                        "addedDate": datetime.datetime.now()
+                    })
